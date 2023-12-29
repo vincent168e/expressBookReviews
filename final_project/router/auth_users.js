768d41d33a5b5e23e1419440961ac761f167aafb
Vincent <vincent168e@gmail.com>
2023-12-29 06:01:57 -0500

    task 7 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,14 +9,31 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password)=>{
+    let validUsers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    if(validUsers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in."});
+    }
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
+        req.session.authorization = {accessToken, username};
+        return res.status(200).json({message: "User successfully logged in."});
+    }
+    return res.status(208).json({message: "Invalid Login. Check username and password."});
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,30 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    let userWithSameName = users.filter((user) => {
+        return (user.username === username);
+    });
+    if (userWithSameName.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if(!doesExist(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registred. Now you can login."});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
