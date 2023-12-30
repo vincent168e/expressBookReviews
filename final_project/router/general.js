126d0adc876e9a0be9253f83dc2a3bdfc19d7d3c
Vincent <vincent168e@gmail.com>
2023-12-30 01:55:05 -0500

    completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,6 +15,7 @@ const doesExist = (username) => {
     }
 }
 
+// 6. Register user
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -30,20 +31,48 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+/*
+// 1. Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    //res.send(JSON.stringify(books, null, 4));
-    //res.send(books);
     return res.status(300).json({message: books});
 });
+*/
 
-// Get book details based on ISBN
+// 10. Get the book list available in the shop (promise callback)
+public_users.get('/', function (req, res) {
+    new Promise(function (resolve, reject) {
+        resolve({status: 300, message: books});
+    }).then(function (data) {
+        return res.status(300).json(data);
+    }).catch(function (err) {
+        console.error(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    });
+});
+
+/*
+// 2. Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     return res.status(300).json({isbn: isbn, message: books[isbn]});
 });
-  
-// Get book details based on author
+*/
+
+// 11. Get book details based on ISBN (promise callback)
+public_users.get('/isbn/:isbn',function (req, res) {
+    new Promise(function (resolve, reject) {
+        const isbn = req.params.isbn;
+        resolve({status: 300, isbn: isbn, message: books[isbn]});
+    }).then(function (data) {
+        return res.status(300).json(data);
+    }).catch(function (err) {
+        console.error(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    });
+});
+
+/*
+// 3. Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let filteredBooks = Object.values(books).filter((book) => {
@@ -51,8 +80,26 @@ public_users.get('/author/:author',function (req, res) {
     });
     return res.status(300).json({message: filteredBooks});
 });
+*/
 
-// Get all books based on title
+// 12. Get book details based on author (promise callback)
+public_users.get('/author/:author',function (req, res) {
+    new Promise(function (resolve, reject) {
+        const author = req.params.author;
+        let filteredBooks = Object.values(books).filter((book) => {
+            return book.author === author;
+        });
+        resolve({status: 300, message: filteredBooks});
+    }).then(function (data) {
+        return res.status(300).json(data);
+    }).catch(function (err) {
+        console.error(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    });  
+});
+
+/*
+// 4. Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let filteredBooks = Object.values(books).filter((book) => {
@@ -60,8 +107,25 @@ public_users.get('/title/:title',function (req, res) {
     });
     return res.status(300).json({message: filteredBooks});
 });
+*/
 
-//  Get book review
+//13. Get all books based on title (promise callback)
+public_users.get('/title/:title', function (req, res) {
+    new Promise(function (resolve, reject) {
+        const title = req.params.title;
+        let filteredBooks = Object.values(books).filter((book) => {
+            return book.title === title;
+        });
+        resolve({status: 300, message: filteredBooks});
+    }).then(function (data) {
+        return res.status(300).json(data);
+    }).catch(function (err) {
+        console.error(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    });
+});
+
+//  5. Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     let book = books[isbn];
